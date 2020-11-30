f48ae45df1065aa17e61bd444d3b61c75eb6297a
Evgeniya Timofeeva <timofeeva.zhenya.1995@gmail.com>
2020-11-30 23:31:15 +0300

    LES-51 Add setUsers function and install axios library

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,8 +3,36 @@ import { Card, Avatar, Button } from 'antd'
 import styled from 'styled-components'
 
 export const Users = ({ usersArr, follow, unfollow, setUsers }) => {
-  if (usersArr.lenght === 0) {
-    setUsers()
+  if (usersArr.length === 0) {
+    setUsers([
+      {
+        id: 0,
+        followed: true,
+        firstName: 'Jems',
+        lastName: 'Band',
+        description: "I'm a frontend developer",
+        avatarUrl:
+          'https://sun9-8.userapi.com/o4uD1PeHB2-m8Db3_tPtHnc4MjmDO4Tv4YF4xw/KT8-N1tVDSY.jpg',
+      },
+      {
+        id: 1,
+        followed: false,
+        firstName: 'Nyuta',
+        lastName: 'Len',
+        description: "I'm a backend developer",
+        avatarUrl:
+          'https://sun9-38.userapi.com/s2Q9Sx9JrvmrGNf4VLI3Q1K3PLq7wdgtyP6tpw/MJU6_Gke5xk.jpg',
+      },
+      {
+        id: 2,
+        followed: false,
+        firstName: 'Rob',
+        lastName: 'Stark',
+        description: "I'm a full-stack developer",
+        avatarUrl:
+          'https://sun9-25.userapi.com/iygXKWZTY6JJq7MHonu84abE2JNwtvyeVtBH3Q/37ktCVugVA0.jpg',
+      },
+    ])
   }
   const renderSubscribeButton = (user) => {
     return user.followed ? (
@@ -27,6 +55,7 @@ export const Users = ({ usersArr, follow, unfollow, setUsers }) => {
       </Button>
     )
   }
+  console.log(usersArr)
 
   return (
     <div>
