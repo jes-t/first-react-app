d889124bc3304c44dabc29872141f6ca5e2eaa14
Evgeniya Timofeeva <timofeeva.zhenya.1995@gmail.com>
2020-12-20 22:40:22 +0300

    [LES-56] Moved functional logic to container

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,67 +1,15 @@
-import React, { useEffect } from 'react'
-import { Card, Avatar, Button, Pagination } from 'antd'
+import React from 'react'
+import { Card, Avatar, Pagination } from 'antd'
 import styled from 'styled-components'
-import * as axios from 'axios'
-import userPhoto from '../../userLogo.png'
 
 export const Users = ({
   usersArr,
-  follow,
-  unfollow,
-  setUsers,
-  setCurrentPage,
   pageSize,
   totalUsersCount,
-  currentPage,
-  setTotalUsersCount,
+  onPageChanged,
+  renderSubscribeButton,
+  renderUserPhoto,
 }) => {
-  useEffect(() => {
-    if (usersArr.length === 0) {
-      axios
-        .get(
-          `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`
-        )
-        .then((response) => {
-          setUsers(response.data.items)
-          setTotalUsersCount(response.data.totalCount)
-        })
-    }
-  }, [])
-  const onPageChanged = (pageNumber) => {
-    setCurrentPage(pageNumber)
-    axios
-      .get(
-        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`
-      )
-      .then((response) => {
-        setUsers(response.data.items)
-      })
-  }
-
-  const renderSubscribeButton = (user) => {
-    return user.followed ? (
-      <Button
-        type="dashed"
-        onClick={() => {
-          unfollow(user.id)
-        }}
-      >
-        Отписаться
-      </Button>
-    ) : (
-      <Button
-        type="dashed"
-        onClick={() => {
-          follow(user.id)
-        }}
-      >
-        Подписаться
-      </Button>
-    )
-  }
-  const renderUserPhoto = (user) => {
-    return user.photos.small != null ? user.photos.small : userPhoto
-  }
   let pageCount = Math.ceil(totalUsersCount / pageSize)
   let pages = []
   for (let i = 1; i <= pageCount; i++) {
@@ -70,7 +18,6 @@ export const Users = ({
   const showTotal = () => {
     return `Всего ${totalUsersCount} пользователей`
   }
-
   return (
     <div>
       <Pagination
--- src/components/Users/UsersContainer.js
+++ src/components/Users/UsersContainer.js
@@ -1,4 +1,4 @@
-import { Users } from './Users'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import {
   followAC,
@@ -7,6 +7,80 @@ import {
   setCurrentPageAC,
   setTotalUsersCountAC,
 } from '../../redux/users-reducer'
+import * as axios from 'axios'
+import { Users } from './Users'
+import { Button } from 'antd'
+import userPhoto from '../../userLogo.png'
+
+export const UsersAPIComponent = ({
+  usersArr,
+  follow,
+  unfollow,
+  setUsers,
+  setCurrentPage,
+  pageSize,
+  totalUsersCount,
+  currentPage,
+  setTotalUsersCount,
+}) => {
+  useEffect(() => {
+    if (usersArr.length === 0) {
+      axios
+        .get(
+          `https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`
+        )
+        .then((response) => {
+          setUsers(response.data.items)
+          setTotalUsersCount(response.data.totalCount)
+        })
+    }
+  }, [])
+  const onPageChanged = (pageNumber) => {
+    setCurrentPage(pageNumber)
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${pageSize}`
+      )
+      .then((response) => {
+        setUsers(response.data.items)
+      })
+  }
+  const renderSubscribeButton = (user) => {
+    return user.followed ? (
+      <Button
+        type="dashed"
+        onClick={() => {
+          unfollow(user.id)
+        }}
+      >
+        Отписаться
+      </Button>
+    ) : (
+      <Button
+        type="dashed"
+        onClick={() => {
+          follow(user.id)
+        }}
+      >
+        Подписаться
+      </Button>
+    )
+  }
+  const renderUserPhoto = (user) => {
+    return user.photos.small != null ? user.photos.small : userPhoto
+  }
+
+  return (
+    <Users
+      usersArr={usersArr}
+      pageSize={pageSize}
+      totalUsersCount={totalUsersCount}
+      onPageChanged={onPageChanged}
+      renderSubscribeButton={renderSubscribeButton}
+      renderUserPhoto={renderUserPhoto}
+    />
+  )
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -27,4 +101,4 @@ const mapDispatchToProps = {
 export const UsersContainer = connect(
   mapStateToProps,
   mapDispatchToProps
-)(Users)
+)(UsersAPIComponent)
