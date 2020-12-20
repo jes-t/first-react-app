24f9388c89b470e175f63a833f0fef5999a5a2bb
Evgeniya Timofeeva <timofeeva.zhenya.1995@gmail.com>
2020-12-21 02:37:34 +0300

    [LES-57] Added preloader for the Users component.

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Card, Avatar, Pagination } from 'antd'
+import { Card, Avatar, Pagination, Spin, Button } from 'antd'
+import userPhoto from '../../userLogo.png'
 import styled from 'styled-components'
 
 export const Users = ({
@@ -7,8 +8,9 @@ export const Users = ({
   pageSize,
   totalUsersCount,
   onPageChanged,
-  renderSubscribeButton,
-  renderUserPhoto,
+  isFetching,
+  follow,
+  unfollow,
 }) => {
   let pageCount = Math.ceil(totalUsersCount / pageSize)
   let pages = []
@@ -18,8 +20,37 @@ export const Users = ({
   const showTotal = () => {
     return `Всего ${totalUsersCount} пользователей`
   }
+
+  const renderSubscribeButton = (user) => {
+    return user.followed ? (
+      <Button
+        type="dashed"
+        disabled={isFetching}
+        onClick={() => {
+          unfollow(user.id)
+        }}
+      >
+        Отписаться
+      </Button>
+    ) : (
+      <Button
+        type="dashed"
+        disabled={isFetching}
+        onClick={() => {
+          follow(user.id)
+        }}
+      >
+        Подписаться
+      </Button>
+    )
+  }
+
+  const renderUserPhoto = (user) => {
+    return user.photos.small != null ? user.photos.small : userPhoto
+  }
+
   return (
-    <div>
+    <RootContainer>
       <Pagination
         defaultCurrent={1}
         total={pageCount}
@@ -28,23 +59,44 @@ export const Users = ({
         showTotal={showTotal}
         showSizeChanger={false}
       />
-      {usersArr.map((user) => {
-        return (
-          <div>
-            <Card style={{ width: 300, marginTop: 16 }} key={user.id}>
-              <Card.Meta
-                avatar={<Avatar size={70} src={renderUserPhoto(user)} />}
-                title={`${user.name}`}
-                description={user.status}
-              />
-            </Card>
-            <SubscribeButton>{renderSubscribeButton(user)}</SubscribeButton>
-          </div>
-        )
-      })}
-    </div>
+      <UsersListContainer>
+        {isFetching && (
+          <SpinContainer>
+            <Spin tip="Loading..." size="large" />{' '}
+          </SpinContainer>
+        )}
+        <UsersList isFetching={isFetching}>
+          {usersArr.map((user) => {
+            return (
+              <div>
+                <Card style={{ width: 300, marginTop: 16 }} key={user.id}>
+                  <Card.Meta
+                    avatar={<Avatar size={70} src={renderUserPhoto(user)} />}
+                    title={`${user.name}`}
+                    description={user.status}
+                  />
+                </Card>
+                <SubscribeButton>{renderSubscribeButton(user)}</SubscribeButton>
+              </div>
+            )
+          })}
+        </UsersList>
+      </UsersListContainer>
+    </RootContainer>
   )
 }
 const SubscribeButton = styled.div`
   padding-top: 5px;
+`
+const SpinContainer = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`
+const RootContainer = styled.div``
+const UsersListContainer = styled.div`
+  position: relative;
+`
+const UsersList = styled.div`
+  opacity: ${(props) => (props.isFetching ? 0.5 : 1)};
 `
