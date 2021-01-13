a50c5c2993db9358860c5a1036378a79046f9176
Evgeniya Timofeeva <timofeeva.zhenya.1995@gmail.com>
2021-01-14 01:03:18 +0400

    [LES-75] Add Form component for authorize.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import { LoginForm } from './LoginForm'
 
 const i18n = {
   authTitle: 'Please authorize.',
@@ -8,9 +9,13 @@ const i18n = {
 
 export const Login = ({ login, isAuth }) => {
   return (
-    <Container>
-      {isAuth ? login : <Link to="/login">{i18n.authTitle}</Link>}
-    </Container>
+    <div>
+      <Container>
+        {isAuth ? login : <Link to="/login">{i18n.authTitle}</Link>}
+      </Container>
+
+      <LoginForm />
+    </div>
   )
 }
 const Container = styled.div`
---
+++ src/components/Login/LoginForm.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Form, Input, Button, Checkbox } from 'antd'
+import { UserOutlined, LockOutlined } from '@ant-design/icons'
+import styled from 'styled-components'
+
+export const LoginForm = (props) => {
+  const onSubmit = (values) => {
+    debugger
+    console.log('Received values of form: ', values)
+  }
+  return (
+    <StyledForm initialValues={{ remember: true }} onFinish={onSubmit}>
+      <Form.Item
+        name="username"
+        rules={[{ required: true, message: 'Please input your Username!' }]}
+      >
+        <Input prefix={<UserOutlined />} placeholder="Username" />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'Please input your Password!' }]}
+      >
+        <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+      </Form.Item>
+    </StyledForm>
+  )
+}
+const StyledForm = styled(Form)`
+  max-width: 300px;
+
+  .login-form-button {
+    width: 100%;
+  }
+`
