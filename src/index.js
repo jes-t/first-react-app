a897b2cd81d0cc7d5a6e2c3fdc02638109dbf58e
Evgeniya Timofeeva <timofeeva.zhenya.1995@gmail.com>
2021-02-03 03:56:53 +0400

    [LES-95] Deploy the project on the site.

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import { BrowserRouter, withRouter } from 'react-router-dom'
+import { HashRouter, withRouter } from 'react-router-dom'
 import { store } from './redux/redux-store'
 import { Provider } from 'react-redux'
 
 const AppWithRouter = withRouter((props) => <App {...props} />)
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <AppWithRouter />
     </Provider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 )
