1bd8e2347e93aa49e6b22c7dc23287cbcd0336df
Evgeniya Timofeeva <timofeeva.zhenya.1995@gmail.com>
2021-02-03 02:49:02 +0400

    Updates

---
+++ precache-manifest.df0e99ab8e3d984626795a4f8ad657cf.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d866e5c98ee5b880a2b3f9cec3edfbef",
+    "url": "/first-react-app/index.html"
+  },
+  {
+    "revision": "b3cc1a6a1e0e258a89f4",
+    "url": "/first-react-app/static/css/3.77123363.chunk.css"
+  },
+  {
+    "revision": "15581ba2c1099a00d690",
+    "url": "/first-react-app/static/css/main.cdfc8296.chunk.css"
+  },
+  {
+    "revision": "5a4034a6b97d93801b6e",
+    "url": "/first-react-app/static/js/0.fde77d41.chunk.js"
+  },
+  {
+    "revision": "b3cc1a6a1e0e258a89f4",
+    "url": "/first-react-app/static/js/3.9929969a.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/first-react-app/static/js/3.9929969a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45a927d2cf17f4b62aab",
+    "url": "/first-react-app/static/js/4.5fa16211.chunk.js"
+  },
+  {
+    "revision": "1889b35f0d228a6b1fe7",
+    "url": "/first-react-app/static/js/5.08f4f1e9.chunk.js"
+  },
+  {
+    "revision": "dda3ef8758dcab9379e8",
+    "url": "/first-react-app/static/js/6.b4d7934b.chunk.js"
+  },
+  {
+    "revision": "2424c82bc34dc12dce6e",
+    "url": "/first-react-app/static/js/7.7942e925.chunk.js"
+  },
+  {
+    "revision": "66340e1aba1cd41b0280",
+    "url": "/first-react-app/static/js/8.3604bf51.chunk.js"
+  },
+  {
+    "revision": "b08677a16f86f4fff8ed",
+    "url": "/first-react-app/static/js/9.111b4ae1.chunk.js"
+  },
+  {
+    "revision": "15581ba2c1099a00d690",
+    "url": "/first-react-app/static/js/main.6f5efddf.chunk.js"
+  },
+  {
+    "revision": "a8f02542fa84ef2c6ecf",
+    "url": "/first-react-app/static/js/runtime-main.2cebfd69.js"
+  },
+  {
+    "revision": "33f5e1fe0d09663658ee543769750c1a",
+    "url": "/first-react-app/static/media/logo.33f5e1fe.png"
+  },
+  {
+    "revision": "08e6ccaf214ba8d9a7f4f9d0294df585",
+    "url": "/first-react-app/static/media/userLogo.08e6ccaf.png"
+  }
+]);
